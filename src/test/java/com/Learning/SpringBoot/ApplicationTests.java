284f2d9fecb7aed3225ff7a6ab0b116ef9a16c37
akhilaakula <akhilaadp@gmail.com>
2025-01-28 22:27:14 -0800

    gradle files

---
+++ src/test/java/com/Learning/SpringBoot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Learning.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
