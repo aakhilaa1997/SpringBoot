f7405b46469bdcda4f65e5cafaadbe3ed533a2e5
akhilaakula <akhilaadp@gmail.com>
2025-01-29 21:43:51 -0800

    First Spring Application Run

--- src/main/java/com/Learning/SpringBoot/Application.java
+++ src/main/java/com/Learning/SpringBoot/Application.java
@@ -2,12 +2,21 @@ package com.Learning.SpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
+	@RequestMapping("/message")
+	public void message(){
+		System.out.println("The Spring Application has started!");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		Application obj = new Application();
+		obj.message();
 	}
 
 }
